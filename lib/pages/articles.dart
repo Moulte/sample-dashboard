84eaa6ed228cebc392e20a201d7549d31b7aa28a
cedric multeau <cedric.multeau@gmail.com>
2025-12-16 23:56:30 +0100

    fix dart issue

--- lib/pages/articles.dart
+++ lib/pages/articles.dart
@@ -43,7 +43,7 @@ class ArticleListPage extends ConsumerWidget {
                 child: ListView.separated(
                   padding: const EdgeInsets.symmetric(horizontal: 16),
                   itemCount: filtered.length,
-                  separatorBuilder: (_, __) => const SizedBox(height: 8),
+                  separatorBuilder: (_, _) => const SizedBox(height: 8),
                   itemBuilder: (context, index) {
                     final a = filtered[index];
                     return Card(
