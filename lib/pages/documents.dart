113bc5e90a2d2951f8769c07f42fa89c2ae76365
cedric multeau <cedric.multeau@gmail.com>
2025-12-16 23:58:15 +0100

    fix dart issue

--- lib/pages/documents.dart
+++ lib/pages/documents.dart
@@ -87,7 +87,7 @@ class DocumentListPage extends ConsumerWidget {
                 child: ListView.separated(
                   padding: const EdgeInsets.symmetric(horizontal: 16),
                   itemCount: filtered.length,
-                  separatorBuilder: (_, __) => const SizedBox(height: 8),
+                  separatorBuilder: (_, _) => const SizedBox(height: 8),
                   itemBuilder: (context, index) {
                     final d = filtered[index];
                     final isExpanded = expandedDocs.contains(d.numeroDocument);
