6e7bcaaee13674d3916442c8e5b7e0afbc993b2f
cedric multeau <cedric.multeau@gmail.com>
2025-12-18 23:10:41 +0100

    fix bug web

--- lib/utils/file_saver.dart
+++ lib/utils/file_saver.dart
@@ -1,17 +1,7 @@
 import 'dart:typed_data';
 
-import 'file_saver_stub.dart'
-    if (dart.library.html) 'file_saver_web.dart'
-    if (dart.library.io) 'file_saver_desktop.dart';
+import 'file_saver_stub.dart' if (dart.library.html) 'file_saver_web.dart' if (dart.library.io) 'file_saver_desktop.dart';
 
-Future<void> saveFile({
-  required Uint8List bytes,
-  required String filename,
-  String mimeType = 'application/octet-stream',
-}) {
-  return saveFileImpl(
-    bytes: bytes,
-    filename: filename,
-    mimeType: mimeType,
-  );
+Future<void> saveFile({required Uint8List bytes, required String filename, String mimeType = 'application/octet-stream'}) {
+  return saveFileImpl(bytes: bytes, filename: filename, mimeType: mimeType);
 }
--- lib/utils/file_saver_web.dart
+++ lib/utils/file_saver_web.dart
@@ -1,15 +1,28 @@
-import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:web/web.dart' as web;
+// import 'package:web/web.dart' as web;
 
-Future<void> saveFileImpl(Uint8List bytes, String filename) async {
-  final web.HTMLAnchorElement anchor = web.document.createElement('a') as web.HTMLAnchorElement
-    ..href = "data:application/octet-stream;base64,${base64Encode(bytes)}"
+// Future<void> saveFileImpl(Uint8List bytes, String filename) async {
+//   final web.HTMLAnchorElement anchor = web.document.createElement('a') as web.HTMLAnchorElement
+//     ..href = "data:application/octet-stream;base64,${base64Encode(bytes)}"
+//     ..style.display = 'none'
+//     ..download = filename;
+
+//   web.document.body!.appendChild(anchor);
+//   anchor.click();
+//   web.document.body!.removeChild(anchor);
+// }
+
+import 'dart:html' as html;
+
+Future<void> saveFileImpl({required Uint8List bytes, required String filename, required String mimeType}) async {
+  final blob = html.Blob([bytes], mimeType);
+  final url = html.Url.createObjectUrlFromBlob(blob);
+
+  html.AnchorElement(href: url)
+    ..download = filename
     ..style.display = 'none'
-    ..download = filename;
+    ..click();
 
-  web.document.body!.appendChild(anchor);
-  anchor.click();
-  web.document.body!.removeChild(anchor);
+  html.Url.revokeObjectUrl(url);
 }
